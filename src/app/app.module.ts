6188cb040eb1aa3a6e6cc7b25949355571454ad2
Benelhadj Dj. Mama Saadia <51034305+SouaadMa@users.noreply.github.com>
2023-02-16 18:42:49 +0100

    Starter files & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './layout/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductItemDetailsComponent } from './components/product-item-details/product-item-details.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductItemDetailsComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product-item/product-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent {
+
+
+  @Input() product: Product | null;
+
+  constructor() {
+    this.product = new Product();
+  }
+
+  ngOnInit(): void {
+  }
+
+  addToCart(arg0: Product|null) {
+    throw new Error('Method not implemented.');
+  }
+
+}
