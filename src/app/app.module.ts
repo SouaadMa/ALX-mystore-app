4f76306a443a9517ecb856b1ca9dfbebadf9a8cc
Benelhadj Dj. Mama Saadia <51034305+SouaadMa@users.noreply.github.com>
2023-02-21 22:09:30 +0100

    feat: checkout form + validation + error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     MatIconModule,
     MatToolbarModule,
--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { CartService } from 'src/app/services/cart.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -9,10 +11,42 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class CheckoutComponent {
   cartItems: Product[] = [];
+  total: number = 0;
+  checkoutForm: FormGroup = new FormGroup({});
 
-  constructor(private cartService: CartService) {}
+  router: Router;
+
+  constructor(
+    private cartService: CartService,
+    router: Router,
+    private fb: FormBuilder
+  ) {
+    this.router = router;
+    this.checkoutForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', [Validators.required, Validators.minLength(3)]],
+      card: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(6),
+          Validators.pattern('[0-9]*'),
+        ],
+      ],
+    });
+  }
 
   ngOnInit(): void {
     this.cartItems = this.cartService.getProducts();
+    this.total = this.cartService.getTotalPrice();
+  }
+
+  onSubmit() {
+    this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.cartItems);
+    // navigate to confirmation page
+    this.router.navigate(['/confirmation/' + this.checkoutForm.value.name]);
   }
 }
