12c0c1d0f4292ea4475f6854c7780bc443411336
Benelhadj Dj. Mama Saadia <51034305+SouaadMa@users.noreply.github.com>
2023-02-21 22:13:28 +0100

    feat: Purchase success page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ProductItemDetailsComponent } from './components/product-item-details/p
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'cart', component: CartComponent },
   { path: 'wishlist', component: WishlistComponent },
   { path: 'products/:id', component: ProductItemDetailsComponent },
-  { path: 'checkout', component: CheckoutComponent }
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'confirmation/:name', component: ConfirmationComponent },
 ];
 
 @NgModule({
--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent {
+
+  name: string = '';
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.name = this.route.snapshot.paramMap.get('name') as string;
+  }
 
 }
